42eafe27c08b03372c4c1233017d3cd0c012bc81
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 20:47:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,38 +6,38 @@ require "pry"
 # or whitespace (' ').
 # class EmailAddressParser
 # attr_accessor :email
-# 
+#
 # def initialize(arg)
 # # binding.pry
 # @email = arg
 # end
-# 
+#
 # def parse
 # @email
 # end
-# 
-# 
-# 
+#
+#
+#
 # end
 
-class EmailParser 
+class EmailParser
   attr_accessor :name, :csv_emails
-  
+
   def initialize(csv_emails)
 #   # you don't need name in here, it is redundant/
 #   @name = name
     @csv_emails = csv_emails
-  end 
+  end
 
-# This will not work because "emails2" below will not be properly parsed  
-# def parse 
+# This will not work because "emails2" below will not be properly parsed
+# def parse
 #   csv_emails.split(/\s*,\s*/)
-# end 
+# end
 
-  def parse 
+  def parse
     csv_emails.split.collect do |address|
-      address.split(',') 
+      address.split(',')
     end
-    .flatten.uniq 
-  end 
-end 
+    .flatten.uniq
+  end
+end
